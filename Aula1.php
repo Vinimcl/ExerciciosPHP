1f4aecfb7fbf2f2c55b79c0369930cc2458b4b5c
Vinícius Maciel <141379362+Vinimcl@users.noreply.github.com>
2024-03-25 20:40:24 -0300

    Add files via upload

---
+++ Aula1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>HOME PAGE</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint ad explicabo nam eum autem quod, nesciunt odit
+        odio quidem asperiores, tenetur sequi qui dolor beatae id ullam neque reiciendis ducimus?</p>
+</body>
+
+</html>
